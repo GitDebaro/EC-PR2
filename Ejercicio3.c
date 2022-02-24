be85a312fd7c194a370d5cc52a72696e0e20ebcb
GitDebaro <91256891+GitDebaro@users.noreply.github.com>
2022-02-24 13:42:08 +0100

    Delete Ejercicio3.c

--- Ejercicio3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    float celsius, farenheit;
-    printf("Escriba el numero de grados Celsius(ºC) a convertir: ");
-    scanf("%f", &celsius);
-    farenheit = (celsius * 1.8) + 32;
-    printf("La temperatura es de %.2f grados Farenheit(ºF) \nFin del algoritmo\n", farenheit);
-    return 0;
-}
